918a1136052e9e84ee8e4d1c3f7b18e18fb667e0
Niklas Kretschmer <niklas.kretschmer@student.reutlingen-university.de>
2023-10-24 15:50:21 +0200

    Grundegrüst angefangen mit Stylesheet und Bildern

--- project/pages/index.js
+++ project/pages/index.js
@@ -1,131 +1,91 @@
-import Head from 'next/head';
-import styles from '../styles/Home.module.css';
+import React from 'react';
+import styles from './style.module.css';
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
-      </Head>
+    <div className={styles.frameDiv}>
+    <section className={[styles.headerSection].join(' ')}>
+      <div className={styles.headerBox}>
+        <img className={[styles.logoIcon].join(' ')} src='./images/Icon.svg'/>
+      </div>
+      <div className={styles.headerBox}>
+        <div>
+          <p>
 
-      <main>
-        <h1 className={styles.title}>
-          Welcome to <a href="https://nextjs.org">Next.js!</a>
-        </h1>
-
-        <p className={styles.description}>
-          Get started by editing <code>pages/index.js</code>
-        </p>
-
-        <div className={styles.grid}>
-          <a href="https://nextjs.org/docs" className={styles.card}>
-            <h3>Documentation &rarr;</h3>
-            <p>Find in-depth information about Next.js features and API.</p>
-          </a>
-
-          <a href="https://nextjs.org/learn" className={styles.card}>
-            <h3>Learn &rarr;</h3>
-            <p>Learn about Next.js in an interactive course with quizzes!</p>
-          </a>
-
-          <a
-            href="https://github.com/vercel/next.js/tree/canary/examples"
-            className={styles.card}
-          >
-            <h3>Examples &rarr;</h3>
-            <p>Discover and deploy boilerplate example Next.js projects.</p>
-          </a>
-
-          <a
-            href="https://vercel.com/import?filter=next.js&utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-            className={styles.card}
-          >
-            <h3>Deploy &rarr;</h3>
-            <p>
-              Instantly deploy your Next.js site to a public URL with Vercel.
-            </p>
-          </a>
+          </p>
         </div>
-      </main>
+        <div>
+          <p>
 
-      <footer>
-        <a
-          href="https://vercel.com?utm_source=create-next-app&utm_medium=default-template&utm_campaign=create-next-app"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Powered by{' '}
-          <img src="/vercel.svg" alt="Vercel" className={styles.logo} />
-        </a>
-      </footer>
+          </p>
+        </div>
+        <div>
+          <p>
 
-      <style jsx>{`
-        main {
-          padding: 5rem 0;
-          flex: 1;
-          display: flex;
-          flex-direction: column;
-          justify-content: center;
-          align-items: center;
-        }
-        footer {
-          width: 100%;
-          height: 100px;
-          border-top: 1px solid #eaeaea;
-          display: flex;
-          justify-content: center;
-          align-items: center;
-        }
-        footer img {
-          margin-left: 0.5rem;
-        }
-        footer a {
-          display: flex;
-          justify-content: center;
-          align-items: center;
-          text-decoration: none;
-          color: inherit;
-        }
-        code {
-          background: #fafafa;
-          border-radius: 5px;
-          padding: 0.75rem;
-          font-size: 1.1rem;
-          font-family:
-            Menlo,
-            Monaco,
-            Lucida Console,
-            Liberation Mono,
-            DejaVu Sans Mono,
-            Bitstream Vera Sans Mono,
-            Courier New,
-            monospace;
-        }
-      `}</style>
+          </p>
+        </div>
+        <div>
+          <p>
+            
+          </p>
+        </div>
+      </div>
+      <div className={styles.headerBox}>
+        <button className={[styles.registerButton].join(' ')}>
+          Register
+        </button>
+      </div>
+    </section>
+    <section className={[styles.bodySection, styles.imageSection].join(' ')}>
+      
+    </section>  
+    <section className={[styles.bodySection, styles.infoContainer].join(' ')}>
+      <div>
+        <h1 className={[styles.headline1].join(' ')}>
+          Snap photos and share like never before
+        </h1>
+      </div>
+      <div className={[styles.infoBoxContainer].join(' ')}>
+        <div className={[styles.infoBox].join(' ')}>
+          <h2 className={[styles.headline2].join(' ')}>
+            Sed ut perspiciatis
+          </h2>
+          <p className={[styles.infoText].join(' ')}>
+          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.
+          </p>
+        </div>
+        <div className={[styles.infoBox].join(' ')}>
+          <h2 className={[styles.headline2].join(' ')}>
+            Sed ut perspiciatis
+          </h2>
+          <p className={[styles.infoText].join(' ')}>
+          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.
+          </p>
+        </div>
+        <div className={[styles.infoBox].join(' ')}>
+          <h2 className={[styles.headline2].join(' ')}>
+            Sed ut perspiciatis
+          </h2>
+          <p className={[styles.infoText].join(' ')}>
+          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.
+          </p>
+        </div>
+        <div className={[styles.infoBox].join(' ')}>
+          <h2 className={[styles.headline2].join(' ')}>
+            Sed ut perspiciatis
+          </h2>
+          <p className={[styles.infoText].join(' ')}>
+          Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione voluptatem sequi nesciunt. Neque porro quisquam est.
+          </p>
+        </div>
+      </div>
+    </section>
+    <section className={[styles.bodySection].join(' ')}>
 
-      <style jsx global>{`
-        html,
-        body {
-          padding: 0;
-          margin: 0;
-          font-family:
-            -apple-system,
-            BlinkMacSystemFont,
-            Segoe UI,
-            Roboto,
-            Oxygen,
-            Ubuntu,
-            Cantarell,
-            Fira Sans,
-            Droid Sans,
-            Helvetica Neue,
-            sans-serif;
-        }
-        * {
-          box-sizing: border-box;
-        }
-      `}</style>
+    </section>
+    <section className={[styles.bodySection].join(' ')}>
+
+    </section>
     </div>
   );
 }
